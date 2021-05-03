6cf4f1af327e8f56a9e7cdc64714ba18d169cbec
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-03 17:38:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,7 +13,7 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
 //if  (age > 12 && age < 20) { return "You are a teenager" }
 //return "You are not a teenager"
-return (age > 12 && age < 20)
+return (age > 12 && age < 20) ?
 "You are a teenager":
 "You are not a teenager"
 }
